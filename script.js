97fe4d38363fac170ca530fc87ef94176728cb16
Mike111222 <Tipu870@gmail.com>
2023-05-31 20:17:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-// Book class to represent individual books
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
-
-// Library class to manage the collection of books
-// we needed two classes
-class Library {
+// Class representing the book library
+class BookLibrary {
   constructor() {
     // Retrieve books from localStorage or initialize an empty array
     this.books = JSON.parse(localStorage.getItem('books')) || [];
+    // Keep track of the current section being displayed
+    this.currentSection = 'list';
   }
 
-  // Function to render the book list on the page
+  // Function to render the list of books
   renderBooks() {
+    // Get the container for displaying books
     const booksDiv = document.getElementById('books');
+    // Clear the container
     booksDiv.innerHTML = '';
 
     // Iterate over each book in the collection
@@ -28,7 +23,6 @@ class Library {
       // Create spans for title and author
       const titleSpan = document.createElement('span');
       const authorSpan = document.createElement('span');
-
       // Set the text content of the spans
       titleSpan.textContent = `Title: ${book.title}`;
       authorSpan.textContent = `Author: ${book.author}`;
@@ -40,7 +34,6 @@ class Library {
       // Create a remove button
       const removeButton = document.createElement('button');
       removeButton.textContent = 'Remove';
-
       // Add custom attribute to store the book index
       removeButton.setAttribute('data-book-index', index);
 
@@ -54,7 +47,11 @@ class Library {
 
   // Function to add a new book to the collection
   addBook(title, author) {
-    const book = new Book(title, author);
+    // Create a new book object
+    const book = {
+      title,
+      author,
+    };
 
     // Add the book to the collection
     this.books.push(book);
@@ -78,9 +75,10 @@ class Library {
     this.renderBooks();
   }
 
-  // Event handler for the remove button clicks
+  // Event handler for remove button clicks
   handleRemoveButtonClick(event) {
     if (event.target.tagName === 'BUTTON') {
+      // Get the index of the book to remove from the custom attribute
       const bookIndex = event.target.getAttribute('data-book-index');
 
       // Check if the data-book-index attribute exists
@@ -93,7 +91,7 @@ class Library {
     }
   }
 
-  // Event handler for the add book form submission
+  // Event handler for add book form submission
   handleAddBookFormSubmit(event) {
     event.preventDefault();
 
@@ -114,32 +112,96 @@ class Library {
     }
   }
 
-  // Function to set up event listeners
-  setupEventListeners() {
-    const booksContainer = document.getElementById('books');
+  // Event handler for navigation links clicks
+  handleNavigationClick(event) {
+    event.preventDefault();
 
-    // Add event listener for the remove button clicks
-    booksContainer.addEventListener('click', this.handleRemoveButtonClick.bind(this));
+    // Get the section from the clicked link's href attribute
+    const section = event.target.getAttribute('href').substring(1);
 
-    const addBookForm = document.getElementById('add-book-form');
+    // Update the current section
+    this.currentSection = section;
 
-    // Add event listener for the add book form submission
-    addBookForm.addEventListener('submit', this.handleAddBookFormSubmit.bind(this));
+    // Update the navigation and content display
+    this.updateNavigation();
+    this.updateContent();
   }
 
-  // Function to initialize the library
+  // Update the active state of navigation links
+  updateNavigation() {
+    // Get all navigation links
+    const navigationLinks = document.querySelectorAll('nav ul li a');
+
+    // Loop through each link
+    navigationLinks.forEach((link) => {
+      // Get the section from the link's href attribute
+      const section = link.getAttribute('href').substring(1);
+
+      // Check if the section matches the current section
+      if (section === this.currentSection) {
+        // Add the 'active' class to the link
+        link.classList.add('active');
+      } else {
+        // Remove the 'active' class from the link
+        link.classList.remove('active');
+      }
+    });
+  }
+
+  // Update the display of content sections
+  updateContent() {
+    // Get all content sections
+    const sections = document.querySelectorAll('section');
+
+    // Loop through each section
+    sections.forEach((section) => {
+      // Check if the section id matches the current section
+      if (section.id === this.currentSection) {
+        // Display the section
+        section.style.display = 'block';
+      } else {
+        // Hide the section
+        section.style.display = 'none';
+      }
+    });
+  }
+
+  // Set up event listeners
+  setupEventListeners() {
+    // Event listener for remove button clicks
+    const booksContainer = document.getElementById('books');
+    booksContainer.addEventListener('click', this.handleRemoveButtonClick.bind(this));
+
+    // Event listener for add book form submission
+    const addBookForm = document.getElementById('add-book-form');
+    addBookForm.addEventListener('submit', this.handleAddBookFormSubmit.bind(this));
+
+    // Event listener for navigation links clicks
+    const navigationLinks = document.querySelectorAll('nav ul li a');
+    navigationLinks.forEach((link) => {
+      link.addEventListener('click', this.handleNavigationClick.bind(this));
+    });
+  }
+
+  // Initialize the book library
   initialize() {
     // Render the initial book list on page load
     this.renderBooks();
 
     // Set up event listeners
     this.setupEventListeners();
+
+    // Update the navigation and content display
+    this.updateNavigation();
+    this.updateContent();
   }
 }
 
-// Create an instance of the Library class and initialize it
-const library = new Library();
-library.initialize();
+// Create an instance of the BookLibrary class
+const bookLibrary = new BookLibrary();
+
+// Initialize the book library
+bookLibrary.initialize();
 
 function updateDateTime() {
   const now = new Date();
